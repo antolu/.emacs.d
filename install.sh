0101b1775569dd7d576204ea89cc70ecb2793a0b
Anton Lu <anton@haochen.lu>
2019-07-09 19:51:15 +0200

    add --needed flag to pacman

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ git clone https://github.com/ycm-core/ycmd.git ~/.ycmd
 cd ~/.ycmd
 git submodule update --init --recursive
 
-sudo pacman -Sy go jdk-openjdk
+sudo pacman -Sy --needed go jdk-openjdk
 python3 build.py --clang-completer --java-completer --go-completer
 
