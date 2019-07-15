11deaad6da386c80bae129da6cf98e28e6bf85d5
Anton <anton96lu@gmail.com>
2019-07-15 10:53:31 +0200

    clone .emacs.d to home automagitcally

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ git submodule update --init --recursive
 sudo pacman -Sy --needed go jdk-openjdk
 python3 build.py --clang-completer --java-completer --go-completer
 
+cd
+
+git clone https://github.com/antolu/.emacs.d.git ~/.emacs.d
