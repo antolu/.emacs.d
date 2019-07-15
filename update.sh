7cd6b5965428ee886ca78bf97884c7a1ed76ac69
Anton Lu <anton96lu@gmail.com>
2019-07-15 14:00:32 +0200

    add update script

---
+++ update.sh
@@ -0,0 +1,10 @@
+cd ~/.emacs.d
+
+git reset --hard HEAD
+git pull
+
+WAKATIME_CLI=`sudo find / -name cli.py | grep wakatime`
+
+sed -i "s@WAKATIME_CLI@$WAKATIME_CLI@" ~/.emacs.d/init.el
+sed -i "s@USER_HOME@$HOME@g" ~/.emacs.d/init.el
+
