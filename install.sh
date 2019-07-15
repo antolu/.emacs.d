b3f6df6404616314c5f884110d7fba8628d86fee
Anton Lu <anton96lu@gmail.com>
2019-07-15 11:49:56 +0200

    install elpy dependencies

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ git submodule update --init --recursive
 sudo pacman -Sy --needed go jdk-openjdk
 python3 build.py --clang-completer --java-completer --go-completer
 
+pip install jedi flake8 autopep8 black yapf
+
 cd
 
 git clone https://github.com/antolu/.emacs.d.git ~/.emacs.d
